c25a267a9d9c8a9e292f50b32d15e80bc3314e81
Tobias Jacob <tobi.jacob@gmx.net>
2024-07-30 18:35:53 +0200

    Finished rendering stuff

--- crates/geop-wgpu/src/headless_renderer.rs
+++ crates/geop-wgpu/src/headless_renderer.rs
@@ -5,14 +5,19 @@ use winit::dpi::PhysicalSize;
 
 use crate::pipeline_manager::PipelineManager;
 
-struct HeadlessRenderer {}
+pub struct HeadlessRenderer {
+    pipeline_manager: PipelineManager,
+    queue: wgpu::Queue,
+    device: wgpu::Device,
+    texture_view: wgpu::TextureView,
+    output_buffer: wgpu::Buffer,
+    texture: wgpu::Texture,
+    texture_size: u32,
+    copy_size: wgpu::Extent3d,
+}
 
 impl HeadlessRenderer {
-    pub async fn new(
-        vertices_points: &VertexBuffer,
-        vertices_line: &EdgeBuffer,
-        vertices_triangle: &TriangleBuffer,
-    ) -> Self {
+    pub async fn new() -> Self {
         let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
             #[cfg(not(target_arch = "wasm32"))]
             backends: wgpu::Backends::PRIMARY,
@@ -75,13 +80,37 @@ impl HeadlessRenderer {
         };
         let output_buffer = device.create_buffer(&output_buffer_desc);
 
-        let mut encoder =
-            device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+        // We need to scope the mapping variables so that we can
+        // unmap the buffer
+
+        HeadlessRenderer {
+            pipeline_manager,
+            queue,
+            device,
+            texture_view,
+            output_buffer,
+            texture,
+            texture_size,
+            copy_size: texture_desc.size,
+        }
+    }
+
+    pub async fn render_to_file(
+        &mut self,
+        vertices_points: &VertexBuffer,
+        vertices_line: &EdgeBuffer,
+        vertices_triangle: &TriangleBuffer,
+        file_path: &std::path::Path,
+    ) {
+        let u32_size = std::mem::size_of::<u32>() as u32;
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
         {
             let render_pass_desc = wgpu::RenderPassDescriptor {
                 label: Some("Render Pass"),
                 color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &texture_view,
+                    view: &self.texture_view,
                     resolve_target: None,
                     ops: wgpu::Operations {
                         load: wgpu::LoadOp::Clear(wgpu::Color {
@@ -97,55 +126,54 @@ impl HeadlessRenderer {
                 timestamp_writes: None,
                 occlusion_query_set: None,
             };
+
             let mut render_pass = encoder.begin_render_pass(&render_pass_desc);
-            pipeline_manager.update_edges(&queue, vertices_line);
-            pipeline_manager.update_triangles(&queue, vertices_triangle);
-            pipeline_manager.update_vertices(&queue, vertices_points);
-            pipeline_manager.run_pipelines(&mut render_pass);
+            self.pipeline_manager
+                .update_edges(&self.queue, vertices_line);
+            self.pipeline_manager
+                .update_triangles(&self.queue, vertices_triangle);
+            self.pipeline_manager
+                .update_vertices(&self.queue, vertices_points);
+            self.pipeline_manager.run_pipelines(&mut render_pass);
         }
+
         encoder.copy_texture_to_buffer(
             wgpu::ImageCopyTexture {
                 aspect: wgpu::TextureAspect::All,
-                texture: &texture,
+                texture: &self.texture,
                 mip_level: 0,
                 origin: wgpu::Origin3d::ZERO,
             },
             wgpu::ImageCopyBuffer {
-                buffer: &output_buffer,
+                buffer: &self.output_buffer,
                 layout: wgpu::ImageDataLayout {
                     offset: 0,
-                    bytes_per_row: Some(u32_size * texture_size),
-                    rows_per_image: Some(texture_size),
+                    bytes_per_row: Some(u32_size * self.texture_size),
+                    rows_per_image: Some(self.texture_size),
                 },
             },
-            texture_desc.size,
+            self.copy_size,
         );
-        queue.submit(Some(encoder.finish()));
+        self.queue.submit(Some(encoder.finish()));
 
-        // We need to scope the mapping variables so that we can
-        // unmap the buffer
-        {
-            let buffer_slice = output_buffer.slice(..);
+        let buffer_slice = self.output_buffer.slice(..);
 
-            // NOTE: We have to create the mapping THEN device.poll() before await
-            // the future. Otherwise the application will freeze.
-            let (tx, rx) = futures_intrusive::channel::shared::oneshot_channel();
-            buffer_slice.map_async(wgpu::MapMode::Read, move |result| {
-                tx.send(result).unwrap();
-            });
-            device.poll(wgpu::Maintain::Wait);
-            rx.receive().await.unwrap().unwrap();
+        // NOTE: We have to create the mapping THEN device.poll() before await
+        // the future. Otherwise the application will freeze.
+        let (tx, rx) = futures_intrusive::channel::shared::oneshot_channel();
+        buffer_slice.map_async(wgpu::MapMode::Read, move |result| {
+            tx.send(result).unwrap();
+        });
+        self.device.poll(wgpu::Maintain::Wait);
+        rx.receive().await.unwrap().unwrap();
 
-            let data = buffer_slice.get_mapped_range();
+        let data = buffer_slice.get_mapped_range();
 
-            use image::{ImageBuffer, Rgba};
-            let buffer =
-                ImageBuffer::<Rgba<u8>, _>::from_raw(texture_size, texture_size, data).unwrap();
-            buffer.save("image.png").unwrap();
-        }
-        output_buffer.unmap();
-
-        HeadlessRenderer {}
+        use image::{ImageBuffer, Rgba};
+        let buffer =
+            ImageBuffer::<Rgba<u8>, _>::from_raw(self.texture_size, self.texture_size, data)
+                .unwrap();
+        buffer.save(file_path).unwrap();
     }
 }
 
@@ -165,10 +193,16 @@ mod tests {
         let vertex_buffer = rasterize_volume_into_vertex_list(&volume, [0.2, 0.2, 0.2, 1.0]);
         let edge_buffer = rasterize_volume_into_line_list(&volume, [0.0, 0.0, 0.0, 1.0]);
         let triangle_buffer = rasterize_volume_into_face_list(&volume, [0.6, 0.6, 0.6, 1.0]);
-        let _headless_renderer = pollster::block_on(HeadlessRenderer::new(
-            &vertex_buffer,
-            &edge_buffer,
-            &triangle_buffer,
-        ));
+        let _headless_renderer = pollster::block_on(async {
+            let mut renderer = HeadlessRenderer::new().await;
+            renderer
+                .render_to_file(
+                    &vertex_buffer,
+                    &edge_buffer,
+                    &triangle_buffer,
+                    std::path::Path::new("test.png"),
+                )
+                .await;
+        });
     }
 }
