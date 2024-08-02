04c4bdfa51702c308401cebc1aaa46f964423d01
Tobias Jacob <tobi.jacob@gmx.net>
2024-08-02 20:58:36 +0200

    Restructuring

--- crates/modern-brep-kernel-book/src/SUMMARY.md
+++ crates/modern-brep-kernel-book/src/SUMMARY.md
@@ -1,5 +1,6 @@
 # Summary
 
-- [Chapter 1 Overview](./chapter_1_overview.md)
-- [Chapter 2 Geometry](./chapter_2_geometry.md)
-- [Chapter 3 Topology](./chapter_3_topology.md)
+- [Overview](./chapter_1_overview.md)
+- [Geometry](./chapter_2_geometry.md)
+    - [Curves](./chapter_2_geometry_curves.md)
+- [Topology](./chapter_3_topology.md)
--- crates/modern-brep-kernel-book/src/graphics/geometry/intersections.rs
+++ crates/modern-brep-kernel-book/src/graphics/geometry/intersections.rs
@@ -108,21 +108,25 @@ mod tests {
 
     #[rstest]
     async fn test_circle_line_intersection(#[future] renderer: Box<HeadlessRenderer>) {
-        let circle1 = primitive_circle(Point::new(0.7, 0.3, 0.0), Point::new_unit_z(), 0.3);
-        let circle2 = primitive_circle(Point::new(0.4, 0.3, 0.0), Point::new_unit_z(), 0.6);
-        let circle3 = primitive_circle(Point::new(-0.4, 0.3, 0.0), Point::new_unit_z(), 0.8);
+        let circle1 = primitive_circle(Point::new(0.4, 0.3, 0.0), Point::new_unit_z(), 0.6);
+        let line1 = primitive_infinite_line(Point::new(-1.0, 0.3, 0.0), Point::new(1.0, 0.3, 0.0));
+        let line2 = primitive_infinite_line(Point::new(-1.0, 0.0, 0.0), Point::new(1.0, 0.0, 0.0));
+        let line3 = primitive_infinite_line(Point::new(-1.0, 0.9, 0.0), Point::new(1.0, 0.9, 0.0));
+        let line4 = primitive_infinite_line(Point::new(-1.0, 1.0, 0.0), Point::new(1.0, 1.0, 0.0));
 
         let mut scene_edges = vec![
             (circle1.clone(), Color::white()),
-            (circle2.clone(), Color::white()),
-            (circle3.clone(), Color::white()),
+            (line1.clone(), Color::white()),
+            (line2.clone(), Color::white()),
+            (line3.clone(), Color::white()),
+            (line4.clone(), Color::white()),
         ];
         let mut scene_points = vec![];
         for intersection in [
-            edge_edge_intersection(&circle1, &circle1),
-            edge_edge_intersection(&circle2, &circle3),
-            edge_edge_intersection(&circle1, &circle3),
-            edge_edge_intersection(&circle1, &circle2),
+            edge_edge_intersection(&circle1, &line1),
+            edge_edge_intersection(&circle1, &line2),
+            edge_edge_intersection(&circle1, &line3),
+            edge_edge_intersection(&circle1, &line4),
         ] {
             match intersection {
                 EdgeEdgeIntersection::Edges(edges) => {
@@ -146,9 +150,7 @@ mod tests {
             .render_to_file(
                 &scene,
                 false,
-                std::path::Path::new(
-                    "src/generated_images/geometry/circle_circle_intersections.png",
-                ),
+                std::path::Path::new("src/generated_images/geometry/circle_line_intersections.png"),
             )
             .await;
     }
