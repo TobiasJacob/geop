dd4d600a0793c7a71ce1955d3e5f74e48d6f35d1
Tobias Jacob <tobi.jacob@gmx.net>
2024-08-04 11:29:43 +0200

    Added contour

--- crates/modern-brep-kernel-book/src/chapter_3_topology_contours.md
+++ crates/modern-brep-kernel-book/src/chapter_3_topology_contours.md
@@ -1,1 +1,11 @@
 # Contours
+
+A contour is a connected vector of edges. It is used to define the boundary of a face. A contour has to be closed, meaning that the first and last edge have to be the same. 
+
+```rust
+pub struct Contour {
+    pub edges: Vec<Edge>,
+}
+```
+
+![Contour](./generated_images/topology/contours.png)
--- crates/modern-brep-kernel-book/src/graphics/topology/contours.rs
+++ crates/modern-brep-kernel-book/src/graphics/topology/contours.rs
@@ -2,7 +2,7 @@
 mod tests {
     use geop_geometry::points::point::Point;
     use geop_topology::{
-        primitive_objects::edges::arc::primitive_arc,
+        primitive_objects::edges::{arc::primitive_arc, line::primitive_line},
         topology::scene::{Color, Scene},
     };
     use geop_wgpu::headless_renderer::HeadlessRenderer;
@@ -11,46 +11,34 @@ mod tests {
     use crate::tests::renderer;
 
     #[rstest]
-    async fn test_arcs(#[future] renderer: Box<HeadlessRenderer>) {
-        let edge = primitive_arc(
-            Point::new(0.0, 0.0, 0.0),
-            Point::new(1.0, 0.0, 0.0),
-            0.6,
-            Point::new_unit_z(),
-        );
-        let edge2 = primitive_arc(
-            Point::new(1.0, 0.0, 0.0),
-            Point::new(-1.0, 1.0, 0.0),
-            1.6,
-            Point::new_unit_z(),
-        );
-        let edge3 = primitive_arc(
-            Point::new(-1.0, 1.0, 0.0),
-            Point::new(0.0, 0.0, 0.0),
-            1.6,
-            Point::new_unit_z(),
-        );
-        let scene = Scene::new(
-            vec![],
-            vec![],
-            vec![
-                (edge, Color::white()),
-                (edge2, Color::white()),
-                (edge3, Color::white()),
-            ],
-            vec![
-                (Point::new(0.0, 0.0, 0.0), Color::green()),
-                (Point::new(1.0, 0.0, 0.0), Color::green()),
-                (Point::new(-1.0, 1.0, 0.0), Color::green()),
-            ],
-        );
+    async fn test_contour(#[future] renderer: Box<HeadlessRenderer>) {
+        let p1 = Point::new(-1.0, 0.0, 1.0);
+        let p2 = Point::new(-1.0, 0.0, -1.0);
+        let p3 = Point::new(1.0, 0.0, -1.0);
+        let p4 = Point::new(1.0, 0.0, 1.0);
+
+        let mut scene = Scene::new(vec![], vec![], vec![], vec![]);
+
+        for p in &[p1, p2, p3, p4] {
+            scene.points.push((p.clone(), Color::red()));
+        }
+
+        for (p1, p2) in &[(p1, p2), (p2, p3), (p3, p4)] {
+            scene.edges.push((primitive_line(*p1, *p2), Color::white()));
+        }
+
+        scene.edges.push((
+            primitive_arc(p4, p1, 1.6, -Point::new_unit_y()),
+            Color::white(),
+        ));
+
         renderer
             .await
             .render_to_file(
                 &scene,
                 false,
                 false,
-                Point::new(0.0, -2.0, 1.0),
+                Point::new(0.0, -4.0, 0.0),
                 std::path::Path::new("src/generated_images/topology/contours.png"),
             )
             .await;
